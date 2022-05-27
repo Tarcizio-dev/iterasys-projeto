00e4a07ab1d8ce8fb627d4a5c95cb013c916529c
Tarciziodev <tarcizio.lima10@gmail.com>
2022-05-27 15:07:32 -0400

    retirei o modo extenso e inclui o bom dia no começo da impressao de texto no console

--- src/main/java/intro/Medidas.java
+++ src/main/java/intro/Medidas.java
@@ -4,39 +4,24 @@ package intro;
 // 2 - referencia as bibliotecas
 
 //3 - Classe
-
-import java.sql.SQLOutput;
-
 public class Medidas {
     // 3.1 - Atributos - Caracteristicas
 
 
     //3.2 - Métodos e Funções
     public static void main(String[] args){
+        System.out.println("Bom dia");
         System.out.println("Calculo de Areas!");
 
+        // Calculo de area - reduzido
+            int largura = 4;
+            int comprimento = 3;
 
-
+            System.out.println("Para a largura de " + largura + " e o comprimento de " + comprimento + " a area é de " + largura * comprimento+ " m2");
 
     }
-    public void calcularAreaModoExtenso({
-        // calculo de area - exemplo: o tamanho do tapete ou do piso
-        int largura;
-        int comprimento;
-        int resultado;
 
-        largura = 4;
-        comprimento = 3;
-
-        resultado = largura * comprimento;
-        System.out.println("Para a largura de " + largura + " e o comprimento de " + comprimento + " a area é de " + resultado + " m2");
-    })
-
-    public void calcularAreaModoCompacto(){
-        int largura = 4;
-        int comprimento = 3;
-
-        System.out.println("Para a largura de " + largura + " e o comprimento de " + comprimento + " a area é de " + largura * comprimento+ " m2")
-    }
 
 }
+
+
