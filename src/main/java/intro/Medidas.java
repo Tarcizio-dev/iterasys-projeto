a0a79b36846a931cb29ca929c1592450ae507463
Tarciziodev <tarcizio.lima10@gmail.com>
2022-05-27 15:20:33 -0400

    exclui o bom dia
    criei o metodo calcularAreaModoCurto e movi o codigo que estava em main para la
    
    o metodo main agora chama os 2 metodos calcular* que passam tambem a ser static

--- src/main/java/intro/Medidas.java
+++ src/main/java/intro/Medidas.java
@@ -10,15 +10,37 @@ public class Medidas {
 
     //3.2 - Métodos e Funções
     public static void main(String[] args){
+
+        calcularAreaModoCurto();
+        calcularAreaModoExtenso();
+
+
+    }
+
+    public static void calcularAreaModoCurto(){
         System.out.println("Bom dia");
-        System.out.println("Calculo de Areas!");
+        System.out.println("Calculo de Area Modo Curto");
 
         // Calculo de area - reduzido
-            int largura = 4;
-            int comprimento = 3;
+        int largura = 6;
+        int comprimento = 5;
 
-            System.out.println("Para a largura de " + largura + " e o comprimento de " + comprimento + " a area é de " + largura * comprimento+ " m2");
+        System.out.println("Para a largura de " + largura + " e o comprimento de " + comprimento + " a area é de " + largura * comprimento+ " m2");
 
+    }
+    public static void calcularAreaModoExtenso(){
+
+        System.out.println("Calculo de Area Modo Extenso");
+        // calculo de area - exemplo: o tamanho do tapete ou do piso
+        int largura;
+        int comprimento;
+        int resultado;
+
+        largura = 4;
+        comprimento = 3;
+
+        resultado = largura * comprimento;
+        System.out.println("Para a largura de " + largura + " e o comprimento de " + comprimento + " a area é de " + resultado + " m2");
     }
 
 
